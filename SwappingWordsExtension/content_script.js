b7617a8d6826c1a7caa1af3c4b4309dd123ca901
Blake Bunting <bbuntin@uwo.ca>
2020-11-21 13:25:16 -0500

    added prompt

--- SwappingWordsExtension/content_script.js
+++ SwappingWordsExtension/content_script.js
@@ -1,9 +1,12 @@
+let person = prompt("What word do you want swapped");
+
+
 const text = document.querySelectorAll('h1,h2,h3,h4,h5,p,li,td,caption,span,a');
 
 for(let i=0;i<text.length;i++){
-    if(text[i].innerHTML.includes('Donald Trump')){
-        text[i].innerHTML = text[i].innerHTML.replace('Donald Trump', 'The Big Orange Sus Man');
-    }else if(text[i].innerHTML.includes('Trump')){
-        text[i].innerHTML = text[i].innerHTML.replace('Trump', 'The Big Orange Sus Man');
+    if(text[i].innerHTML.includes(person)){
+        text[i].innerHTML = text[i].innerHTML.replace(person, 'The Big Orange Sus Man');
+    //}else if(text[i].innerHTML.includes(person)){
+        //text[i].innerHTML = text[i].innerHTML.replace(person, 'The Big Orange Sus Man');
     }
 }

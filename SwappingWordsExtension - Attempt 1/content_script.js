b507a0c40286ea99afc84a9d092339250ec70e90
elliotjwarner <ewarner8@uwo.ca>
2020-11-21 16:08:14 -0500

    got alert working

--- SwappingWordsExtension - Attempt 1/content_script.js
+++ SwappingWordsExtension - Attempt 1/content_script.js
@@ -1,26 +1,9 @@
-let swapout;
-let swapin;
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse){
-        swapout = request.swapout;
-        swapin = request.swapin;
+function show(swapout,swapin){
+    const text = document.querySelectorAll('h1,h2,h3,h4,h5,p,li,td,caption,span,a');
 
-        if(request.swapout || request.swapin){
-            sendResponse({wasPassed: "Failed:("})
-        }else{
-            sendResponse({wasPassed: "Values passed!"})
+    for(let i=0;i<text.length;i++){
+        if(text[i].innerHTML.includes(swapout)){
+            text[i].innerHTML = text[i].innerHTML.replace(swapout, swapin);
         }
-    })
-
-
-
-
-const text = document.querySelectorAll('h1,h2,h3,h4,h5,p,li,td,caption,span,a');
-
-for(let i=0;i<text.length;i++){
-    if(text[i].innerHTML.includes(swapout)){
-        text[i].innerHTML = text[i].innerHTML.replace(swapout, swapin);
-    }else if(text[i].innerHTML.includes(swapout)){
-        text[i].innerHTML = text[i].innerHTML.replace(swapout, swapin);
     }
 }
--- SwappingWordsExtension - Attempt 1/popup.js
+++ SwappingWordsExtension - Attempt 1/popup.js
@@ -1,15 +1,11 @@
+//import { show } from "./content_script.js";
 document.addEventListener('DOMContentLoaded',documentEvents, false);
 
 function documentEvents(){
     document.getElementById('bttn').addEventListener('click',function () {
-        myAction(document.getElementById('swappedout'),document.getElementById('swappedin'));
-    });
-}
-
-function myAction(leaving,coming){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-        chrome.tabs.sendMessage(tabs[0].id,{swapout: leaving,swapin: coming}, function(response){
-
-        })
+        let swapout = document.getElementById('swappedout').value;
+        let swapin = document.getElementById('swappedin').value;
+        alert(swapout, swapin);
+        //show(swapout,swapin);
     });
 }

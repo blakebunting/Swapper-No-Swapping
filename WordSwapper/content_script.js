756b59ece57093eeb3cd34b54fc992e2f434f30f
elliotjwarner <ewarner8@uwo.ca>
2020-11-21 21:09:51 -0500

    refresh

--- WordSwapper/content_script.js
+++ WordSwapper/content_script.js
@@ -1,12 +1,22 @@
+let image = document.createElement('img'); 
+
+
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 
+    //Word Swap
     const text = document.querySelectorAll('h1,h2,h3,h4,h5,p,li,td,caption,span,a');
-
-    //not sure how to change request into swapin and swapout
     for(let i=0;i<text.length;i++){
-        if(text[i].innerHTML.includes(request.swappedout)){
+        if(text[i].innerHTML.toLowerCase().includes(request.swappedout.toLowerCase())){
             text[i].innerHTML = text[i].innerHTML.replace(request.swappedout, request.swappedin);
         }
     }
-  }
-);
+
+    //Image Swap
+    image.src = request.picture;
+    const img = document.querySelectorAll('img');
+    for(let i=0;i<img.length;i++){
+        img[i].src =  image.src;
+        img[i].srcset = image.src;
+        console.log(img[i]);
+    }
+});
--- WordSwapper/popup.js
+++ WordSwapper/popup.js
@@ -2,6 +2,13 @@ document.getElementById('bttn').addEventListener('click',function () {
     chrome.tabs.query({ active: true }, function(tabs) {
         let swapOut = document.getElementById('swappedout').value;
         let swapIn = document.getElementById('swappedin').value;
-        chrome.tabs.sendMessage(tabs[0].id, { "swappedout": swapOut, "swappedin": swapIn });
+        let pic = document.getElementById('pic').value;
+        chrome.tabs.sendMessage(tabs[0].id, { "swappedout": swapOut, "swappedin": swapIn , "picture" : pic});
     })
 });
+
+window.addEventListener("load", event => {
+    document.getElementById("reload").onclick = function(){
+        location.reload(true);
+    }
+})
